60fa265e89583011143ab41fe2826813d46bd93f
Jacek J. Lakis <mlody3k@gmail.com>
2026-03-15 16:33:05 +0100

    no ns registration

--- bindings/python/tests/python/server.py
+++ bindings/python/tests/python/server.py
@@ -22,6 +22,7 @@ def main():
            'lookup': clientlookup,
            'query': clientquery,
            'registerevents': client_register_events}
+    
     my_result = foo.init(args, map)
     print("Testing PMIx_Initialized")
     rc = foo.initialized()
@@ -35,18 +36,6 @@ def main():
 
     # get our environment as a base
     env = os.environ.copy()
-    # register an nspace for the client app
-    (rc, regex) = foo.generate_regex(["test000","test001","test002"])
-    print("Node regex, rc: ", regex, rc)
-    (rc, ppn) = foo.generate_ppn(["0,1,2", "3,4,5", "6,7"])
-    print("PPN, rc: ", ppn, rc)
-    kvals = [{'key':PMIX_NODE_MAP, 'value':regex, 'val_type':PMIX_STRING},
-             {'key':PMIX_PROC_MAP, 'value':ppn, 'val_type':PMIX_STRING},
-             {'key':PMIX_UNIV_SIZE, 'value':1, 'val_type':PMIX_UINT32},
-             {'key':PMIX_JOB_SIZE, 'value':1, 'val_type':PMIX_UINT32}]
-    print("REGISTERING NSPACE")
-    rc = foo.register_nspace("testnspace", 1, kvals)
-    print("RegNspace ", rc)
 
     # register a client
     uid = os.getuid()
