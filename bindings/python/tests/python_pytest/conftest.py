c746fe37e54108303e4347a5877be3c661b13349
Jacek J. Lakis <mlody3k@gmail.com>
2026-05-21 20:34:15 +0200

    runs, no test yet

--- bindings/python/tests/python_pytest/conftest.py
+++ bindings/python/tests/python_pytest/conftest.py
@@ -2,6 +2,9 @@
 Pytest fixtures for PMIx python bindings tests.
 """
 
+from datetime import datetime
+import os
+
 from server_upcalls import *
 import pytest
 
@@ -9,7 +12,7 @@ import pytest
 @pytest.fixture(scope="module")
 def server():
     """ Configure PMIx Server """
-    print("Initializing PMIxServer...")
+    print("Server: Initializing PMIxServer...")
     try:
         server = PMIxServer()
     except Exception as e:
@@ -33,21 +36,49 @@ def server():
         'query': clientquery,
         'registerevents': client_register_events
     }
-    
+
     _ = server.init(server_init_args, server_callbacks)
     is_initialized = server.initialized()
 
     assert is_initialized, "PMIx Server failed to initialize"
-    print("PMIxServer initialized.")
+    print("Server: PMIxServer initialized.")
 
     yield server
     print("Finalizing PMIxServer...")
     del server
 
-# Function scope - once per test function
-@pytest.fixture(scope="function")
-def client(server):
+# Also module scope - initializes once and runs sequential chain of tests
+# simple_clients = world of size 1
+@pytest.fixture(scope="module")
+def simple_client(server):
     """ Configure PMIx Client """
-    print(f"Initializing PMIxClient... {server}")
+    # Generate randon ns name from date - 2026_06_11_15_30_45_123456
+    namespace_name = f"namespace_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f')}"
+
+    # Register namespace
+    (rc, regex) = server.generate_regex(["test000","test001","test002"])
+    print("Client: Node regex, rc: ", regex, rc)
+    (rc, ppn) = server.generate_ppn(["0,1,2", "3,4,5", "6,7"])
+    print("Client: PPN, rc: ", ppn, rc)
+    kvals = [{'key':PMIX_NODE_MAP, 'value':regex, 'val_type':PMIX_STRING},
+             {'key':PMIX_PROC_MAP, 'value':ppn, 'val_type':PMIX_STRING},
+             {'key':PMIX_UNIV_SIZE, 'value':1, 'val_type':PMIX_UINT32},
+             {'key':PMIX_JOB_SIZE, 'value':1, 'val_type':PMIX_UINT32}]
+    print(f"Client: Registering namespace {namespace_name}...")
+    rc = server.register_nspace(namespace_name, 1, kvals)
+    print(f"Client: Register namespace return code: {server.error_string(rc)}")
+
+    # register a client
+    env = os.environ.copy()    
+    uid = os.getuid()
+    gid = os.getgid()
+
+    rc = server.register_client({'nspace':namespace_name, 'rank':0}, uid, gid)
+    print(f"Client: Register client return code: {server.error_string(rc)}")
+
+    # setup the fork
+    rc = server.setup_fork({'nspace':namespace_name, 'rank':0}, env)
+    print(f"Client: Setup fork return code: {server.error_string(rc)}")
+
     yield None
     print("Finalizing PMIxClient...")
--- bindings/python/tests/python_pytest/test_client_kv.py
+++ bindings/python/tests/python_pytest/test_client_kv.py
@@ -1,16 +1,16 @@
 """ Tests for client KV operations """
 
 
-def test_put(client):
+def test_put(simple_client):
     print("Test put")
     pass
 
 
-def test_commit(client):
+def test_commit(simple_client):
     print("Test commit")
     pass
 
 
-def test_get(client):
+def test_get(simple_client):
     print("Test get")
     pass
