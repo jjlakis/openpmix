15daa7ac131d94748c53f8f3659a2d18df118a58
Jacek J. Lakis <mlody3k@gmail.com>
2026-05-21 20:21:38 +0200

    something working

--- bindings/python/tests/python_pytest/README.md
+++ bindings/python/tests/python_pytest/README.md
@@ -25,3 +25,15 @@ pytest -sv test_client_kv.py
 ```
 pytest -sv test_client_kv.py::TestClientKV::test_put
 ```
+
+## Run locally in Docker
+
+From this directory:
+
+```
+# Build image once:
+docker build . -t test
+
+# Then, for testing, just mount the working directory in the proper place
+docker run --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -e PMIX_MCA_gds=hash -e PMIX_MCA_psec=none -it --name server -v $(pwd):/test --workdir /test test bash
+```
--- bindings/python/tests/python_pytest/conftest.py
+++ bindings/python/tests/python_pytest/conftest.py
@@ -1,17 +1,53 @@
+"""
+Pytest fixtures for PMIx python bindings tests.
+"""
+
+from server_upcalls import *
 import pytest
 
-# Module scope - once per file
+# Server - module scope - once per file
 @pytest.fixture(scope="module")
 def server():
     """ Configure PMIx Server """
     print("Initializing PMIxServer...")
-    yield None
+    try:
+        server = PMIxServer()
+    except Exception as e:
+        raise RuntimeError(f"FAILED TO CREATE SERVER: {e}")
+
+    server_init_args = [
+        {'key':'FOOBAR', 'value':'VAR', 'val_type':PMIX_STRING},
+        {'key':'BLAST', 'value':7, 'val_type':PMIX_INT32},
+        {'key': 'pmix.tcp.ipv4', 'value': 3333, 'val_type': PMIX_INT},
+        {'key': 'pmix.tcp.disipv6', 'value': True,  'val_type': PMIX_BOOL},
+        {'key': 'pmix.tcp.repuri',  'value': '-',   'val_type': PMIX_STRING},
+    ]
+
+    server_callbacks = {
+        'clientconnected': clientconnected,
+        'clientfinalized': clientfinalized,
+        'fencenb': clientfence,
+        'publish': clientpublish,
+        'unpublish': clientunpublish,
+        'lookup': clientlookup,
+        'query': clientquery,
+        'registerevents': client_register_events
+    }
+    
+    _ = server.init(server_init_args, server_callbacks)
+    is_initialized = server.initialized()
+
+    assert is_initialized, "PMIx Server failed to initialize"
+    print("PMIxServer initialized.")
+
+    yield server
     print("Finalizing PMIxServer...")
+    del server
 
 # Function scope - once per test function
 @pytest.fixture(scope="function")
 def client(server):
     """ Configure PMIx Client """
-    print("Initializing PMIxClient...")
+    print(f"Initializing PMIxClient... {server}")
     yield None
     print("Finalizing PMIxClient...")
